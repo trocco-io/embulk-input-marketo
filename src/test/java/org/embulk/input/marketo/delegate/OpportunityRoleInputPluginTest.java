175c372a449dded22c7d359be12fd85d3589b17b
maeken <js.maeken.o@gmail.com>
2024-03-06 07:44:18 +0000

    add OpportunityRoleInputPluginTest

--- src/test/java/org/embulk/input/marketo/delegate/OpportunityRoleInputPluginTest.java
+++ src/test/java/org/embulk/input/marketo/delegate/OpportunityRoleInputPluginTest.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.embulk.input.marketo.MarketoUtilsTest.CONFIG_MAPPER;
-import static org.embulk.input.marketo.delegate.OpportunityInputPlugin.PluginTask;
+import static org.embulk.input.marketo.delegate.OpportunityRoleInputPlugin.PluginTask;
 import static org.junit.Assert.assertArrayEquals;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
@@ -36,14 +36,14 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class OpportunityInputPluginTest
+public class OpportunityRoleInputPluginTest
 {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @Rule
     public EmbulkTestRuntime embulkTestRuntime = new EmbulkTestRuntime();
 
-    private OpportunityInputPlugin opportunityInputPlugin;
+    private OpportunityRoleInputPlugin opportunityRoleInputPlugin;
 
     private ConfigSource configSource;
 
@@ -52,39 +52,39 @@ public class OpportunityInputPluginTest
     @Before
     public void setUp() throws Exception
     {
-        opportunityInputPlugin = spy(new OpportunityInputPlugin());
+        opportunityRoleInputPlugin = spy(new OpportunityRoleInputPlugin());
         ConfigLoader configLoader = embulkTestRuntime.getInjector().getInstance(ConfigLoader.class);
-        configSource = configLoader.fromYaml(this.getClass().getResourceAsStream("/config/opportunity.yaml"));
+        configSource = configLoader.fromYaml(this.getClass().getResourceAsStream("/config/opportunity_role.yaml"));
         mockMarketoRestClient = mock(MarketoRestClient.class);
-        doReturn(mockMarketoRestClient).when(opportunityInputPlugin).createMarketoRestClient(any(PluginTask.class));
+        doReturn(mockMarketoRestClient).when(opportunityRoleInputPlugin).createMarketoRestClient(any(PluginTask.class));
     }
 
     @Test
     public void testValidPluginTask()
     {
         PluginTask pluginTask = mapTask(configSource);
-        opportunityInputPlugin.validateInputTask(pluginTask);
+        opportunityRoleInputPlugin.validateInputTask(pluginTask);
     }
 
     @Test
-    public void testOpportunityFilterTypeError()
+    public void testOpportunityRoleFilterTypeError()
     {
-        PluginTask pluginTask = mapTask(configSource.set("opportunity_filter_type", ""));
-        Assert.assertThrows(ConfigException.class, () -> opportunityInputPlugin.validateInputTask(pluginTask));
+        PluginTask pluginTask = mapTask(configSource.set("opportunity_role_filter_type", ""));
+        Assert.assertThrows(ConfigException.class, () -> opportunityRoleInputPlugin.validateInputTask(pluginTask));
     }
 
     @Test
     public void testEmptyStringFilterValues()
     {
-        PluginTask pluginTask = mapTask(configSource.set("opportunity_filter_values", ""));
-        Assert.assertThrows(ConfigException.class, () -> opportunityInputPlugin.validateInputTask(pluginTask));
+        PluginTask pluginTask = mapTask(configSource.set("opportunity_role_filter_values", ""));
+        Assert.assertThrows(ConfigException.class, () -> opportunityRoleInputPlugin.validateInputTask(pluginTask));
     }
 
     @Test
     public void testAllEmptyStringFilterValues()
     {
-        PluginTask pluginTask = mapTask(configSource.set("opportunity_filter_values", ",, , "));
-        Assert.assertThrows(ConfigException.class, () -> opportunityInputPlugin.validateInputTask(pluginTask));
+        PluginTask pluginTask = mapTask(configSource.set("opportunity_role_filter_values", ",, , "));
+        Assert.assertThrows(ConfigException.class, () -> opportunityRoleInputPlugin.validateInputTask(pluginTask));
     }
 
     @Test
@@ -92,16 +92,16 @@ public class OpportunityInputPluginTest
     {
         RecordPagingIterable<ObjectNode> mockRecordPagingIterable = mock(RecordPagingIterable.class);
         JavaType javaType = OBJECT_MAPPER.getTypeFactory().constructParametrizedType(List.class, List.class, ObjectNode.class);
-        List<ObjectNode> objectNodeList = OBJECT_MAPPER.readValue(this.getClass().getResourceAsStream("/fixtures/opportunity_response.json"), javaType);
+        List<ObjectNode> objectNodeList = OBJECT_MAPPER.readValue(this.getClass().getResourceAsStream("/fixtures/opportunity_role_response.json"), javaType);
         when(mockRecordPagingIterable.iterator()).thenReturn(objectNodeList.iterator());
-        when(mockMarketoRestClient.getOpportunities(anyString(), anyString())).thenReturn(mockRecordPagingIterable);
+        when(mockMarketoRestClient.getOpportunityRoles(anyString(), anyString())).thenReturn(mockRecordPagingIterable);
 
         PluginTask task = mapTask(configSource);
-        ServiceResponseMapper<? extends ValueLocator> mapper = opportunityInputPlugin.buildServiceResponseMapper(task);
+        ServiceResponseMapper<? extends ValueLocator> mapper = opportunityRoleInputPlugin.buildServiceResponseMapper(task);
         RecordImporter recordImporter = mapper.createRecordImporter();
         PageBuilder mockPageBuilder = mock(PageBuilder.class);
-        opportunityInputPlugin.ingestServiceData(task, recordImporter, 1, mockPageBuilder);
-        verify(mockMarketoRestClient, times(1)).getOpportunities(anyString(), anyString());
+        opportunityRoleInputPlugin.ingestServiceData(task, recordImporter, 1, mockPageBuilder);
+        verify(mockMarketoRestClient, times(1)).getOpportunityRoles(anyString(), anyString());
 
         Schema schema = mapper.getEmbulkSchema();
         ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
